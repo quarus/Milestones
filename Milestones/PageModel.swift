544f0c8afb7bae7f5ac7864cd5813b17232e57a0
Altay Cebe <altay.cebe@icloud.com>
2018-12-22 20:33:06 +0100

    adds license text

--- Milestones/PageModel.swift
+++ Milestones/PageModel.swift
@@ -1,6 +1,10 @@
 //
-//  InfiniteScrollViewModel.swift
-//  Milestones
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// ArrowGraphic.swift
+// Milestones
 //
 //  Created by Altay Cebe on 09.12.18.
 //  Copyright © 2018 Altay Cebe. All rights reserved.
