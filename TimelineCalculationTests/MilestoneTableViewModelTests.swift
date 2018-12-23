e727de2820dd4f8b58d8f7f138f882072dcc8981
Altay Cebe <altay.cebe@icloud.com>
2018-12-23 21:23:39 +0100

    adds code to prepopulate a test managedobjectcontext

--- TimelineCalculationTests/MilestoneTableViewModelTests.swift
+++ TimelineCalculationTests/MilestoneTableViewModelTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 import CoreData
+@testable import Milestones
 
 class DataController: NSObject {
     let persistentContainer: NSPersistentContainer
@@ -30,17 +31,40 @@ class MilestoneTableViewModelTests: XCTestCase {
         print("DataController ready")
     })
  
+    func prePopulate() {
+        let newTimeline = NSEntityDescription.insertNewObject(forEntityName: "Timeline",
+                                                              into: managedObjectContext) as! Timeline
+        newTimeline.name = "Timeline 1"
+        
+        let firstMilestone = NSEntityDescription.insertNewObject(forEntityName: "Milestone",
+                                                                 into: managedObjectContext) as! Milestone
+        firstMilestone.name = "Milestone 1"
+        firstMilestone.date = Date().normalized()
+        firstMilestone.timeline = newTimeline
+        
+        let secondMilestone = NSEntityDescription.insertNewObject(forEntityName: "Milestone",
+                                                                 into: managedObjectContext) as! Milestone
+        secondMilestone.name = "Milestone 2"
+        secondMilestone.date = Date().normalized()
+        secondMilestone.timeline = newTimeline
+
+        let thirdMilestone = NSEntityDescription.insertNewObject(forEntityName: "Milestone",
+                                                                  into: managedObjectContext) as! Milestone
+        thirdMilestone.name = "Milestone 3"
+        thirdMilestone.date = Date().normalized()
+        thirdMilestone.timeline = newTimeline
+    }
+    
     override func setUp() {
         managedObjectContext =  NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
         managedObjectContext.persistentStoreCoordinator = dataController.persistentContainer.persistentStoreCoordinator
+        prePopulate()
     }
 
     override func tearDown() {
     }
     
     func testStub() {
-        let newTimeline = NSEntityDescription.insertNewObject(forEntityName: "Timeline", into: managedObjectContext)
-        let newMilestone = NSEntityDescription.insertNewObject(forEntityName: "Milestone", into: managedObjectContext)
     }
     
     func test2 () {
