c01abb986c75963607dd5976375e09aa47f6c1f9
Altay Cebe <altay.cebe@icloud.com>
2018-12-27 14:48:57 +0100

    updates a license text

--- Milestones/MilestoneTableCellModell.swift
+++ Milestones/MilestoneTableCellModell.swift
@@ -1,6 +1,7 @@
 //
-//  MilestoneCellModell.swift
-//  Milestones
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 //  Created by Altay Cebe on 24.12.18.
 //  Copyright © 2018 Altay Cebe. All rights reserved.
